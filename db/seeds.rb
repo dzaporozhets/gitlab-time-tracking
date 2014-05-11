d02de73049b5922cf55194c15f2cab2d559108a6
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2014-05-11 18:24:44 +0300

    Add seeds
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+dates = (1.weeks.ago.to_date..Date.today).to_a
+times = [0.5, 1, 1.5, 2, 4]
+projects = [1,2,3]
+issues = [1,2,3,4,5]
+
+10.times do
+  TimeLog.create(
+    time: times.sample,
+    day: dates.sample,
+    issue_iid: issues.sample,
+    project_id: projects.sample,
+  )
+end
